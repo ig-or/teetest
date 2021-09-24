f37cf5dee7ab7da42a6292c8a6514d27532c518f
ig-or <19777752+ig-or@users.noreply.github.com>
2021-09-23 22:44:23 -0700

    blink updated

--- test/blink/blink.cpp
+++ test/blink/blink.cpp
@@ -1,13 +1,36 @@
 
 #include <Arduino.h>
+const int ledPin = 13;
+const int maxPWM = 65535;  // because of the 16 bit resolution
 
 extern "C" int main(void) {
-	pinMode(13, OUTPUT);
+  // initialize the digital pin as an output.
+	pinMode(ledPin, OUTPUT);	
+	uint32_t now = millis();
+	int mpw = maxPWM / 2;
+	analogWriteResolution(16);
+	//analogWriteFrequency(ledPin, 915.527);
+	int p = 0;
+	int phase;
 	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(1000);
-		digitalWriteFast(13, LOW);
-		delay(1000);
+		now = millis();
+		phase = (now % 2000);
+		if (phase < 1000) {
+			p = phase * mpw / 1000;
+		} else {
+			p = mpw - (phase - 1000)*mpw / 1000;
+		}
+		if ( p > mpw) {
+			  p = mpw;
+		}
+		//digitalWriteFast(13, HIGH);
+		//delay(1000);
+		//digitalWriteFast(13, LOW);
+		//delay(1000);
+
+
+		analogWrite(ledPin, p);
+		delay(10);
 	}
 
 }
